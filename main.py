c2ce5b026582bc864da2765d1fa5299638c333fb
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-10-20 19:22:32 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Vector collinear analysis library.
+"""
+
+
+def first_condition() -> bool:
+    """
+    a = n * b.
+    """
+    pass
+
+
+def second_condition() -> bool:
+    """
+    ax/bx == ay/by.
+    """
+    pass
+
+
+def third_condition() -> bool:
+    """
+    vec(a*b) == vec(0).
+    """
+    pass
+
+
+if __name__ == '__main__':
+    a = [3, 5, 16, 21]
+    b = [4, 5, 24, 20]
